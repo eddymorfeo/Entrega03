097dde4338b9d42f797b1d4553ff3a851396ea07
Edward Tejeda <edward.tejeda1987@gmail.com>
2023-05-30 18:10:22 -0400

    Entrega 3 VS1

---
+++ main.js
@@ -0,0 +1,5 @@
+let nombreTarea = document.getElementById("nombre");
+let descripcionTarea = document.getElementById("descripcion");
+let fechaTarea = document.getElementById("fecha");
+let estadoTarea = document.getElementById("estado");
+
